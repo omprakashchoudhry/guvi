49d174b4ba619a7cbd631dbc7b3d010bc8610858
omprakashchoudhry <37272375+omprakashchoudhry@users.noreply.github.com>
2019-07-05 09:49:42 +0530

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -1,8 +1,8 @@
-for _ in range(int(input())):
-    x=int(input())
-    if x<0:
-        print("it is negative")
-    elif x==0:
-        print("it is zero")
-    else:
-        print("it is posititve")
+
+x=int(input())
+if x<0:
+    print("it is negative")
+elif x==0:
+    print("it is zero")
+else:
+    print("it is posititve")
